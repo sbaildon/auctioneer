07f852ed7d7333fe2d519d3b80c1a9484d332a96
Sean Baildon <sean@mbp.local>
2013-10-24 20:18:30 +0100

    added basic login GUI

--- src/AuctionClient.java
+++ src/AuctionClient.java
@@ -4,7 +4,8 @@ public class AuctionClient {
 
     public static void main(String args[]) {
         try {
-            Auction a = (Auction) Naming.lookup("rmi://localhost:2020/AuctioneerService");
+            GUI gui = new GUI();
+            //Auction a = (Auction) Naming.lookup("rmi://localhost:2020/AuctioneerService");
         } catch (Exception e) {
             System.out.println("Failed to find RMI");
         }
--- src/GUI.java
+++ src/GUI.java
@@ -6,12 +6,16 @@ public class GUI {
     Frame frame;
 
     public GUI() {
-        this.frame = createWindow("Auction House", 600, 500);
+        this.frame = windowFrame("Auction House", 480, 300);
+        this.frame.add(loginPanel(480, 300));
+        this.frame.setVisible(true);
     }
 
-    public Frame createWindow(String title, int width, int height){
+    private Frame windowFrame(String title, int width, int height){
         Frame frame = new Frame(title);
-        frame.setMinimumSize(new Dimension(width, height));
+        frame.setResizable(false);
+        frame.setSize(new Dimension(width, height));
+        frame.setLocationRelativeTo(null);
 
         frame.addWindowListener(new WindowAdapter() {
             @Override
@@ -20,7 +24,37 @@ public class GUI {
             }
         });
 
-        frame.setVisible(true);
         return frame;
+    }
+
+    private Panel loginPanel(int width, int height) {
+        Panel panel;
+        TextField username, email;
+        Button loginBtn;
+        Label intro;
+
+        intro = new Label("Login");
+        intro.setFont(new Font("Sans Serif", Font.PLAIN, 38));
+        intro.setBounds(((width / 2) - 52), 30, 105, 400);
+
+        username = new TextField(15);
+        username.setBounds(((width / 2) - 65), ((height / 2) - 40), 130, 25);
+
+        email = new TextField(15);
+        email.setBounds(((width / 2) - 65), (height / 2), 130, 25);
+
+        loginBtn = new Button("login");
+        loginBtn.setBounds(((width / 2 ) - 37), (height - (height / 3)), 75, 25);
+
+        panel = new Panel();
+        panel.setLayout(null);
+
+        panel.add(loginBtn);
+        panel.add(username);
+        panel.add(email);
+        panel.add(intro);
+
+        panel.setVisible(true);
+        return panel;
     }
 }
