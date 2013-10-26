08127c0b5b560e51685414298467b08fd33ac2bc
Sean Baildon <sean@sbaildon.com>
2013-10-26 16:04:39 +0100

    added basic login functionality to the GUI

--- src/AuctionClient.java
+++ src/AuctionClient.java
@@ -1,13 +1,50 @@
 import java.rmi.Naming;
 
 public class AuctionClient {
+    public static final AuctionClient client = new AuctionClient();
+    private static Auction a;
+    private static GUI gui;
 
     public static void main(String args[]) {
         try {
-            GUI gui = new GUI();
-            //Auction a = (Auction) Naming.lookup("rmi://localhost:2020/AuctioneerService");
+            a = (Auction) Naming.lookup("rmi://localhost:2020/AuctioneerService");
+            gui = new GUI();
         } catch (Exception e) {
             System.out.println("Failed to find RMI");
+            e.printStackTrace();
         }
     }
+
+    private AuctionClient() {
+    }
+
+    public static AuctionClient getInstance() {
+        return client;
+    }
+
+    public void addUser(String name, String email) {
+        User user;
+        user = new User(name, email);
+        try {
+            if (a.addUser(user)) {
+                gui.provideFeedback("Account created");
+            } else {
+                gui.provideFeedback("Email already in use");
+            }
+        } catch (Exception e) {
+            System.out.println("Failed to create user (serious)");
+        }
+    }
+
+    public void login(String name, String email) {
+        User user;
+        user = new User(name, email);
+        try {
+            if (a.login(user)) {
+            }
+        } catch (Exception e) {
+            gui.provideFeedback("Failed to login");
+        }
+    }
+
 }
--- src/GUI.java
+++ src/GUI.java
@@ -1,11 +1,19 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class GUI {
-    Frame frame;
+public class GUI implements ActionListener {
+    private Panel panel;
+    private TextField username, email;
+    private Button loginBtn, regisBtn;
+    private Label intro, info;
+    private Frame frame;
+    private AuctionClient client;
 
     public GUI() {
+        client = client.getInstance();
         this.frame = windowFrame("Auction House", 480, 300);
         this.frame.add(loginPanel(480, 300));
         this.frame.setVisible(true);
@@ -28,33 +36,58 @@ public class GUI {
     }
 
     private Panel loginPanel(int width, int height) {
-        Panel panel;
-        TextField username, email;
-        Button loginBtn;
-        Label intro;
-
         intro = new Label("Login");
         intro.setFont(new Font("Sans Serif", Font.PLAIN, 38));
         intro.setBounds(((width / 2) - 52), 30, 105, 400);
 
         username = new TextField(15);
         username.setBounds(((width / 2) - 65), ((height / 2) - 40), 130, 25);
+        username.setText("username");
 
         email = new TextField(15);
         email.setBounds(((width / 2) - 65), (height / 2), 130, 25);
+        email.setText("email");
 
-        loginBtn = new Button("login");
-        loginBtn.setBounds(((width / 2 ) - 37), (height - (height / 3)), 75, 25);
+        loginBtn = new Button("Login");
+        loginBtn.setBounds(((width / 2) - 77), (height - (height / 3)), 75, 25);
+        loginBtn.addActionListener(this);
+
+        regisBtn = new Button("Register");
+        regisBtn.setBounds(((width / 2) + 7), (height - (height / 3)), 75, 25);
+        regisBtn.addActionListener(this);
+
+        info = new Label();
+        info.setFont(new Font("Sans Serif", Font.PLAIN, 12));
+        info.setBounds(((width / 2) - 52), 30, 300, 104);
 
         panel = new Panel();
         panel.setLayout(null);
 
         panel.add(loginBtn);
+        panel.add(regisBtn);
         panel.add(username);
         panel.add(email);
         panel.add(intro);
+        panel.add(info);
 
-        panel.setVisible(true);
         return panel;
+    }
+
+    public void provideFeedback(String message) {
+        info.setText(message);
+        try {
+            Thread.sleep(2000);
+        } catch (Exception e) {
+        }
+        info.setText(null);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == loginBtn) {
+            client.login(username.getText(), email.getText());
+        } else if (e.getSource() == regisBtn) {
+            client.addUser(username.getText(), email.getText());
+        }
     }
 }
